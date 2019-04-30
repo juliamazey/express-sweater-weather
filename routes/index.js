15c3c7c7ec58236c2275c23ecff4344d7e5f10c5
Julia Marco <marcootj@gmail.com>
2019-04-30 17:36:45 -0600

    Added user creation

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('../models');
-// var bcrypt = require('bcrypt');
-// const saltRounds = 10;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
