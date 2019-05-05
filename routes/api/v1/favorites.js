e928b3ef0d553d6d8eafb29ab2e3de844f91878d
Julia Marco <35079289+juliamarco@users.noreply.github.com>
2019-05-05 14:51:23 -0600

    Update favorites.js

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -9,158 +9,151 @@ pry = require('pryjs');
 
 
 // POST favorite for a user
-router.post('/', function(req, res, next) {
-  User.findOne({
-    where: {
-      api_key: req.body.api_key
-    }
-  })
+router.post('/', function(req, res) {
+  User.findOne({where: {api_key: req.body.api_key}})
   .then(function(user) {
     if (user === null) {
-      res.setHeader("Content-Type", "application/json");
-      res.status(401).send(JSON.stringify("Your API key is not valid"));
+      sendInvalidResponse(res)
     }
     else {
-      var location = req.body.location
-  	  var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GEOCODING_API}`
-  	  fetch(url)
-  	  .then(function(location_response){
-  	    return location_response.json();
-  	  })
-  	  .then(function(location_json){
-  	    var lat = location_json.results[0].geometry.location.lat
-        var long = location_json.results[0].geometry.location.lng
-  	    var url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${long}`
-        Location.findOrCreate({
-          where: { address: location},
-          defaults: {
-            latitude: lat,
-            longitude: long }
-        })
-        .then( loc => {
-          return Favorite.findOrCreate({
-            where: {
-              LocationId: loc[0].id,
-              UserId: user.id
-            },
-            defaults: {
-              LocationId: loc[0].id,
-              UserId: user.id
-            }
-          })
-        })
-        .then( fav => {
-          res.setHeader("Content-Type", "application/json");
-          res.status(200).send(JSON.stringify({
-            message: `${location} has been added to your favorites`
-          }));
-        })
-        .catch(error => {
-          res.setHeader("Content-Type", "application/json");
-          res.status(500).send({ error });
-        })
-      });
+      var loc = req.body.location
+  	  var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${loc}&key=${process.env.GEOCODING_API}`
+      getLocation(loc)
+  	  .then(function(fetched_location){
+        return createLocation(fetched_location, loc);
+      })
+      .then( loc => {
+        return addFavoriteResponse(loc, user, res)
+      })
 	  }
   })
   .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({ error });
+    sendErrorResponse(res, error);
   })
 });
 
 // GET favorites for a user
-router.get('/', function(req, res, next) {
-  User.findOne({
-    where: {
-      api_key: req.body.api_key
-    }
-  })
+router.get('/', function(req, res) {
+  User.findOne({where: {api_key: req.body.api_key}})
   .then(function(user) {
     if (user === null) {
-      res.setHeader("Content-Type", "application/json");
-      res.status(401).send(JSON.stringify("Your API key is not valid"));
+      sendInvalidResponse(res)
     }
     else {
       return Location.findAll({
-        include: {
-          model: Favorite,
-          where: { UserId: user.id}
-        },
+        include: { model: Favorite, where: { UserId: user.id} }
       })
       .then( favorites => {
-        var favoritesWeather = favorites.map( function(fav) {
-          lat = fav.latitude
-          long = fav.longitude
-    	    url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${long}`
-          var fetched = fetch(url)
-          .then(function(forecast_response){
-            return forecast_response.json();
-          })
-          .then( weather_data => {
-            return {
-              location: fav.address,
-              current_weather: weather_data.currently
-            }
-          })
-          .then(results => {
-            return results
-          })
-          return fetched
-        });
-        return Promise.all(favoritesWeather)
+        return getFavoritesForecast(favorites);
       })
       .then(favs => {
         res.setHeader("Content-Type", "application/json"),
         res.status(200).send(favs);
       })
       .catch(error => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(500).send({ error });
+        sendErrorResponse(res, error);
       })
 	  }
   })
   .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({ error });
+    sendErrorResponse(res, error);
   })
 });
 
 // DELETE favorite for a user
-router.delete('/', function(req, res, next) {
-  User.findOne({
-    where: {
-      api_key: req.body.api_key
-    }
-  })
+router.delete('/', function(req, res) {
+  User.findOne({where: {api_key: req.body.api_key}})
   .then(function(user) {
     if (user === null) {
-      res.setHeader("Content-Type", "application/json");
-      res.status(401).send(JSON.stringify("Your API key is not valid"));
+      sendInvalidResponse(res)
     }
     else {
-      Location.findOne({where: { address: req.body.location }})
-      .then(location => {
-        Favorite.destroy({
-          where: {
-            UserId: user.id,
-            LocationId: location.id
-          }
-        })
-      })
-      .then(fav => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(204).send();
-      })
-      .catch(error => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(500).send({ error })
-      });
+      destroyLocation(req.body.location, res, user);
     }
   })
   .catch(error => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(500).send({ error });
+    sendErrorResponse(res, error);
   })
 });
 
-	module.exports = router;
+function sendInvalidResponse(res) {
+  res.setHeader("Content-Type", "application/json");
+  res.status(401).send(JSON.stringify("Your API key is not valid"));
+}
+
+function sendErrorResponse(res, error) {
+  res.setHeader("Content-Type", "application/json");
+  res.status(500).send({ error });
+}
+
+function getLocation(location) {
+  var url = `https://maps.googleapis.com/maps/api/geocode/json?address=${location}&key=${process.env.GEOCODING_API}`
+  var fetched = fetch(url)
+  .then(function(location_response){
+    return location_response.json();
+  })
+  return fetched
+};
+
+function createLocation(fetched_location, loc){
+  var lat = fetched_location.results[0].geometry.location.lat
+  var long = fetched_location.results[0].geometry.location.lng
+  var url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${long}`
+  var location = Location.findOrCreate({
+    where: { address: loc},
+    defaults: { latitude: lat, longitude: long }
+  })
+  return location
+};
+
+function addFavoriteResponse(loc, user, res) {
+  return Favorite.findOrCreate({
+    where: { LocationId: loc[0].id, UserId: user.id },
+    defaults: { LocationId: loc[0].id, UserId: user.id}
+  })
+  .then( fav => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).send(JSON.stringify({ message: `${loc[0].address} has been added to your favorites`}));
+  })
+  .catch(error => {
+    sendErrorResponse(res, error);
+  })
+};
+
+function getFavoritesForecast(favorites) {
+  var favoritesWeather = favorites.map( function(fav) {
+    lat = fav.latitude
+    long = fav.longitude
+    url = `https://api.darksky.net/forecast/${process.env.DARK_SKY_API}/${lat},${long}`
+    var fetched = fetch(url)
+    .then(function(forecast_response){
+      return forecast_response.json();
+    })
+    .then( weather_data => {
+      return { location: fav.address, current_weather: weather_data.currently }
+    })
+    .then(results => {
+      return results
+    })
+    return fetched
+  });
+  return Promise.all(favoritesWeather)
+};
+
+function destroyLocation(loc, res, user) {
+  Location.findOne({where: { address: loc }})
+  .then(location => {
+    Favorite.destroy({
+      where: { UserId: user.id, LocationId: location.id}
+    })
+  })
+  .then(fav => {
+    res.setHeader("Content-Type", "application/json");
+    res.status(204).send();
+  })
+  .catch(error => {
+    sendErrorResponse(res, error);
+  })
+};
+
+module.exports = router;
